4d9a4f9eee2a0be8a9f157dcf71c605edb92b0bd
WOLFSMIND\WolfsMind <cameronch1993@gmail.com>
2022-01-18 17:54:21 +1100

    colours defined in colour.js

---
+++ src/settings/colours.js
@@ -0,0 +1,27 @@
+//Faction Colours
+export const colourFremen = '#CA842A';
+export const colourAtreides = '#49642B';
+export const colourHarkonen = '#222326';
+export const colourBeneGesserit = '#1B3366';
+export const colourSpacingGuild = '#C3491E';
+export const colourEmperor = '#8A1D16';
+export const colourTleilaxu = '#2C1A48'; 
+export const colourIxian = '#7C7E82';
+
+//App Background, Headings, and Text
+export const colourHeadingMaj = '#171D29';
+export const colourHeadingMin = '#20334D';
+export const colourMainBG = '#FFFCF0';
+export const colourFooterTrans1 = '#E8D293';
+export const colourFooterTrans2 = '#B6A66C';
+export const colourBGText = '#3F1F19';
+
+//Card Title Backgrounds
+export const colourDarkRed = '#833031';
+export const colourLightRed = '#A43831';
+export const colourDarkPurple = '#2E2A41';
+export const colourLightPurple = '#3D3C5b';
+export const colourDarkTan = '#7E663A';
+export const colourLightTan = '#AF986C';
+export const colourDarkBrown = '#413429';
+export const colourLightBrown = '#73654A';
